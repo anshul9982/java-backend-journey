fcd22bad6ab4842d9f75e278554dfe31ac20b78f
anshul <anshultyagi55562@gmail.com>
2025-06-22 10:26:27 +0530

    day 1 practice

---
+++ src/practice1.java
@@ -0,0 +1,19 @@
+public class practice1 {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 10;
+        if (a > b) {
+            System.out.println("A is greater");
+        } else {
+            System.out.println("B is greater");
+        }
+
+        switch (a) {
+            case 10:
+                System.out.println("Ten");
+                break;
+            default:
+                System.out.println("Other");
+        }
+    }
+}
