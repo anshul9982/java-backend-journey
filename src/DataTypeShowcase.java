4eec127318efd63c43e7c778b61ff5d87bc54dca
anshul <anshultyagi55562@gmail.com>
2025-06-23 17:22:14 +0530

    Implement DataTypeShowcase exercise

---
+++ src/DataTypeShowcase.java
@@ -0,0 +1,21 @@
+public class DataTypeShowcase {
+    public static void main(String[] args) {
+        byte smal = 10;
+        int num = 100;
+        short num2 = 200;
+        long num3 = 100000000L;
+        float dig = 11.22f;
+        double dig2 = 5.61;
+        char car = 'A';
+        boolean istrue = true;
+
+        System.out.println("Byte: " + smal);
+        System.out.println("integer: " + num);
+        System.out.println("short: " + num2);
+        System.out.println("long: " + num3);
+        System.out.println("float: " + dig);
+        System.out.println("double: " + dig2);
+        System.out.println("char: " + car);
+        System.out.println("boolean: " + istrue);
+    }
+}
