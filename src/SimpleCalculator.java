b63d647ae47e9e0b3083e6ef13bf4c3d85fc0212
anshul <anshultyagi55562@gmail.com>
2025-06-24 15:03:27 +0530

    Complete SimpleCalculator with input and operator handling

---
+++ src/SimpleCalculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        try(Scanner scanner = new Scanner(System.in)) {
+            System.out.println("enter number 1 : ");
+            double num1 = scanner.nextDouble();
+            System.out.println("enter number 2 : ");
+            double num2 = scanner.nextDouble();
+            scanner.nextLine();
+            System.out.println("choose your desired operation :");
+            String operation = scanner.nextLine();
+
+            switch (operation){
+                case "+":
+                    System.out.println(num1+num2);
+                    break;
+                case "-":
+                    System.out.println(num1-num2);
+                    break;
+                case "*":
+                    System.out.println(num1*num2);
+                    break;
+                case "/":
+                    if (num2==0){
+                        System.out.println("division by zero not allowed!");
+                    }
+                    else{
+                        System.out.println(num1/num2);
+                    }
+                    break;
+                default:
+                    System.out.println("enter a valid operator");
+            }
+        }
+    }
+}
