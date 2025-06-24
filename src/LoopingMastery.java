a8003439e1102da368cfbd9f525f71a6a954992b
anshul <anshultyagi55562@gmail.com>
2025-06-24 14:21:26 +0530

    loops practice using for, while, do-while

---
+++ src/LoopingMastery.java
@@ -0,0 +1,35 @@
+public class LoopingMastery {
+    public static void main(String[] args) {
+        //for loop:
+        for (int i = 1; i <=10; i++) {
+            if(i==7){
+                break;
+            }
+            else {
+                System.out.println("i :" + i);
+            }
+        }
+        // while loop:
+        int num = 20;
+        while (num>=10 ){
+            System.out.println(num);
+            --num;
+        }
+        //do-while loop:
+        int random;
+        do {
+            random = (int) (Math.random() * 6) + 1;
+            System.out.println("you rolled: " + random);
+        }
+        while (random!=6);
+        //continue keyword:
+        for (int i = 1; i <=5; i++) {
+            if(i==3){
+                continue;
+            }
+            else {
+                System.out.println("i : " + i);
+            }
+        }
+    }
+}
