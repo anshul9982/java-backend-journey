be0a34e96c361e4fa025b4ef0702ad311b46e2d1
anshul <anshultyagi55562@gmail.com>
2025-07-01 23:55:35 +0530

    implemented String Manipulation

--- src/chapter4/StringMethods.java
+++ src/chapter4/StringMethods.java
@@ -38,6 +38,7 @@ public class StringMethods {
         System.out.println(message.charAt(0));
         System.out.println(String.valueOf(123));
         System.out.println(String.valueOf(true));
+        
 
 
     }
